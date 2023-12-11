4e04e726e95495d12ea9090813512dd5be93863e
TeriyakiBeefDipper <jason82329@gmail.com>
2023-12-11 16:30:19 +0800

    added # noqa for flake8

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -17,7 +17,7 @@ class CommandTests(SimpleTestCase):
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """test waiting for database when getting OperationalError"""
-        patched_check.side_effect = [Psycopg2Error] * 2 + [OperationalError] * 3 + [True]
+        patched_check.side_effect = [Psycopg2Error] * 2 + [OperationalError] * 3 + [True]  # noqa
         call_command("wait_for_db")
         self.assertEqual(patched_check.call_count, 6)
         patched_check.assert_called_with(databases=['default'])
