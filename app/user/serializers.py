9de0d4bebf0c8a227935808dbe4733581ebacb81
TeriyakiBeefDipper <jason82329@gmail.com>
2023-12-12 18:10:19 +0800

    create tests for user token, serializers, views, and urls for user token

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,9 +1,11 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 from rest_framework import serializers
+from django.utils.translation import gettext as _
 
 
 class UserSerializer(serializers.ModelSerializer):
     """serialize the user object"""
+
     class Meta:
         model = get_user_model()
         fields = ['email', 'password', 'name']
@@ -14,6 +16,26 @@ class UserSerializer(serializers.ModelSerializer):
         email = validated_data.get('email')
         password = validated_data.get('password')
         name = validated_data.get('name')
-        user = get_user_model().objects.create_user(email=email, password=password, name=name)   # noqa
-        user.set_password(password)
+        user = get_user_model().objects.create_user(email=email, password=password, name=name)  # noqa
+        # user.set_password(password)    # this should be redundant
         return user
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    """Serializer for the user auth token"""
+    email = serializers.EmailField()
+    password = serializers.CharField(style={'input_type': "password"}, trim_whitespace=False)  # noqa
+
+    def validate(self, attrs):
+        """validate and authenticate user"""
+        email = attrs.get('email')
+        password = attrs.get('password')
+        user = authenticate(request=self.context.get('request'),
+                            username=email,
+                            password=password)
+        if not user:
+            msg = _('Unable to authenticate')
+            raise serializers.ValidationError(msg, code='authorization')
+
+        attrs['user'] = user
+        return attrs
--- app/user/views.py
+++ app/user/views.py
@@ -1,7 +1,14 @@
 from rest_framework import generics
-from .serializers import UserSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.serializers import api_settings
+from .serializers import UserSerializer, AuthTokenSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
     """create a new user view"""
     serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
