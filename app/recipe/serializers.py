a77d610d7096e0f85af9f6df1727e0bcb6bda0b3
TeriyakiBeefDipper <jason82329@gmail.com>
2023-12-23 21:34:36 +0800

    added update method for tags in RecipeSerializer, with tests in test_recipe_api.py

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -18,16 +18,31 @@ class RecipeSerializer(serializers.ModelSerializer):
         fields = ('id', 'title', 'time_minutes', 'price', 'link', 'tags')
         read_only_fields = ['id']
 
+    def _get_or_create_tags(self, tags, recipe):
+        """handle getting or creating tags as needed"""
+        auth_user = self.context['request'].user
+        for tag in tags:  # noqa this is moved form create method, and we edit create method to be simpler
+            tag_obj, created = Tag.objects.get_or_create(user=auth_user, **tag)  # noqa
+            recipe.tags.add(tag_obj)
+
     def create(self, validated_data):
         """create a recipe, because tags are nested serializer"""
         tags = validated_data.pop('tags', [])
         recipe = Recipe.objects.create(**validated_data)
         # this might break our code
-        auth_user = self.context['request'].user
-        for tag in tags:
-            tag_obj, created = Tag.objects.get_or_create(user=auth_user, **tag)   # noqa
-            recipe.tags.add(tag_obj)
+        self._get_or_create_tags(tags, recipe)
         return recipe
+
+    def update(self, instance, validated_data):
+        """update recipe"""
+        tags = validated_data.pop('tags', None)
+        if tags is not None:
+            instance.tags.clear()
+            self._get_or_create_tags(tags, instance)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        return instance
 
 
 class RecipeDetailSerializer(RecipeSerializer):
