26a18d556f25b2fedd3e3a5d8b508d8832ff4052
TeriyakiBeefDipper <jason82329@gmail.com>
2023-12-24 13:41:47 +0800

    core/models.py modify recipe model for image field, core/tests/test_models.py

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 from decimal import Decimal
 from .. import models
+from unittest.mock import patch
 
 
 def create_user(email='user@example.com', password='test123'):
@@ -65,3 +66,11 @@ class ModelTests(TestCase):
         user = create_user()
         ingredient = models.Ingredient.objects.create(user=user, name='Ingredient1')  # noqa
         self.assertEqual(str(ingredient), ingredient.name)
+
+    @patch('core.models.uuid.uuid4')
+    def test_recipe_file_name_uuid(self, mock_uuid):
+        """test generating image path"""
+        uuid = 'test-uuid'
+        mock_uuid.return_value = uuid
+        file_path = models.recipe_image_file_path(None, 'example.jpg')
+        self.assertEqual(file_path, f'uploads/recipe/{uuid}.jpg')
