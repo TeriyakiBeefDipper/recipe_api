aef5526d280416188443738c8b57091e126dbfcb
TeriyakiBeefDipper <jason82329@gmail.com>
2023-12-26 21:38:44 +0800

    added proxy configurations

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,12 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c33bup!0a=qs=kgwm$^)$^o14-sqs#q6&wwxwe1%i#l^^okmw#'
+# SECRET_KEY = 'django-insecure-c33bup!0a=qs=kgwm$^)$^o14-sqs#q6&wwxwe1%i#l^^okmw#'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(filter(None,
+                            os.environ.get('ALLOWED_HOST', '').split(',')))
 
 # Application definition
 
